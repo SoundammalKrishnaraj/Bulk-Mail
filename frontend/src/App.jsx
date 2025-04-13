bd2ebe72cf4441b20a796ff4ab28248fd2cbc076
Soundammal Krishnaraj <soundammal.krishnaraj@gmail.com>
2025-04-13 10:46:04 +0530

    Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -38,7 +38,7 @@ function App() {
 
   function send() {
     setstatus(true)
-    axios.post("http://localhost:5000/sendemail", { msg: msg ,emailList:emailList })
+    axios.post("mongodb+srv://Soundammal:Soundammal@cluster0.j4un40c.mongodb.net/passkey?retryWrites=true&w=majority&appName=Cluster0", { msg: msg ,emailList:emailList })
       .then(function (data) {
         if (data.data == true) {
           alert("Email Sent Successfully")
